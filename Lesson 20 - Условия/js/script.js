28af218ab9f6432b87bd0194dcd3b6ec003205d0
Dmytro <dmitry.khandogy@gmail.com>
2023-01-21 14:49:31 +0100

    Create Lesson 20

---
+++ Lesson 20 - Условия/js/script.js
@@ -0,0 +1,17 @@
+"use strict"; //запись для использования строгого стиля кода
+
+if( 4 == 9) {
+    console.log("Ok");
+}
+
+const num = 50;
+
+if(num < 49){
+    console.log('A little');
+} else if(num > 100){
+    console.log('A lot');
+} else {
+    console.log('OK');
+}
+
+(num === 50) ? console.log('OK!');
