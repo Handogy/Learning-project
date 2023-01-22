96f192cc1543d58ba46c15d3d122b1e6c11e509b
Dmytro <dmitry.khandogy@gmail.com>
2023-01-22 14:52:00 +0100

    Add Lesson 21, 22

--- Lesson 20 - Условия/js/script.js
+++ Lesson 20 - Условия/js/script.js
@@ -4,14 +4,39 @@ if( 4 == 9) {
     console.log("Ok");
 }
 
+// const num = 50;
+
+// if(num < 49){
+//     console.log('A little');
+// } else if(num > 100){
+//     console.log('A lot');
+// } else {
+//     console.log('OK');
+// }
+
+// Тернарный оператор
+// (num === 50) ? console.log('OK!') : console.log('Error!');
+
+
+/** 
+ * Switch
+ */
+
 const num = 50;
 
-if(num < 49){
-    console.log('A little');
-} else if(num > 100){
-    console.log('A lot');
-} else {
-    console.log('OK');
-}
+switch (num) {
+    case 49: 
+        console.log('Error!');
+        break;
+    case 100:
+        console.log('Error!');
+        break;
+    case 50:
+        console.log('Ok!');
+        break;
 
-(num === 50) ? console.log('OK!');
+    default:
+        console.log('Not this time!');
+        break;
+
+}
---
+++ Lesson 22 - Циклы/js/script.js
@@ -0,0 +1,10 @@
+"use strict"; //запись для использования строгого стиля кода
+
+const hamburger = true;
+const fries = true;
+
+if(hamburger && fries){
+    console.log('I have eaten!');
+}
+
+console.log(hamburger && fries);
