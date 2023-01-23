e46b9df8c812b2916b5758d391e9acabac9d5ab0
Dmytro <dmitry.khandogy@gmail.com>
2023-01-23 20:29:06 +0100

    Lesson 23 - end

--- Lesson 23 - Цикл в цикле и метки/js/script.js
+++ Lesson 23 - Цикл в цикле и метки/js/script.js
@@ -5,3 +5,80 @@
  */
 
 
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+
+    }
+}
+
+
+// let result = '';
+
+// for (let i = 0; i < 50; i++) {
+//     result += '*';
+
+//     console.log(result);
+// }
+
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i <= length; i++) {
+    
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+
+    result += '\n';
+}
+
+console.log(result);
+
+
+/** 
+ * Метки
+ */
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+    
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue first;
+            console.log(`Third level: ${k}`);
+        }
+    }
+}
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+    
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) break first;
+            console.log(`Third level: ${k}`);
+        }
+    }
+}
+
+
+/**
+ * Homework
+ */
+
+const arrayOfNumbers = [];
+
+// Пишем решение вот тут
+for (var i = 5; i <= 10; i++) {
+    arrayOfNumbers.push(i);
+}
+
+console.log(arrayOfNumbers);
