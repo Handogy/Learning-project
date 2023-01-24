61874f4eb492cbfe356962b884f5a9f71a2364d7
Dmytro <dmitry.khandogy@gmail.com>
2023-01-24 20:11:55 +0100

    Lesson 23 - Homework

--- Lesson 23 - Цикл в цикле и метки/js/script.js
+++ Lesson 23 - Цикл в цикле и метки/js/script.js
@@ -82,3 +82,77 @@ for (var i = 5; i <= 10; i++) {
 }
 
 console.log(arrayOfNumbers);
+
+
+/** 
+ * 23.4
+ */
+
+// 1
+// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// // Пишем решение вот тут
+// for (let i = 0; i < arr.length; i++) {
+//     result.push(arr[i]);
+// }
+
+// console.log(result);
+
+
+// 2
+// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// // Пишем решение вот тут
+// for (let i = 0; i < data.length; i++) {
+//     if( typeof(data[i]) == 'number' ){
+//         data[i] = (data[i] * 2);
+//     } else if(typeof(data[i]) == 'string'){
+//         data[i] = (data[i] + " - done");
+//     }
+// }
+
+// // Не трогаем
+
+// console.log(data);
+
+
+// 3
+// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result2 = [];
+
+// Пишем решение вот тут
+for (let i = data.length - 1; i >= 0; i--) {
+    result2.push(data[i]);
+}
+
+console.log(result2);
+
+// 4
+const lines = 15;
+let result3 = '';
+// Проверяется именно переменная result, формируйте строку в ней
+for (let i = 1; i <= lines; i++) {
+    const countStars = i * 2 - 1;
+    const countLastStars = lines * 2 - 1;
+    const countSideSpaces = (countLastStars - countStars) / 2;
+
+    for (let j = 0; j < countSideSpaces; j++) {
+        result3 += ' ';
+    }
+
+    for (let j = 0; j < countStars; j++) {
+        result3 += '*';
+    }
+
+    for (let j = 0; j < countSideSpaces; j++) {
+        result3 += ' ';
+    }
+
+    result3 += '\n';
+}
+
+console.log(result3);
