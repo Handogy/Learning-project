a232acd0cc4095301cc77d5fcf526d94f6ef1b08
Dmytro <dmitry.khandogy@gmail.com>
2023-01-25 18:29:23 +0100

    Lesson 23 - END

--- Lesson 23 - Цикл в цикле и метки/js/script.js
+++ Lesson 23 - Цикл в цикле и метки/js/script.js
@@ -24,19 +24,19 @@ for (let i = 0; i < 3; i++) {
 // }
 
 
-let result = '';
-const length = 7;
+// let result = '';
+// const length = 7;
 
-for (let i = 1; i <= length; i++) {
+// for (let i = 1; i <= length; i++) {
     
-    for (let j = 0; j < i; j++) {
-        result += '*';
-    }
+//     for (let j = 0; j < i; j++) {
+//         result += '*';
+//     }
 
-    result += '\n';
-}
+//     result += '\n';
+// }
 
-console.log(result);
+// console.log(result);
 
 
 /** 
