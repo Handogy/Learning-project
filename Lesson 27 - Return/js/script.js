4f5f88e2f2374d6cf4e009c4530c1f67d48bf039
Dmytro <dmitry.khandogy@gmail.com>
2023-02-01 21:19:17 +0100

    Lesson 27 homework

--- Lesson 27 - Return/js/script.js
+++ Lesson 27 - Return/js/script.js
@@ -8,11 +8,62 @@
 
 
 const usdCurr = 28;
-const euroCurr = 32;
+const discount = 0.9;
 
 function convert(amount, curr) {
-    console.log(curr * amount);
+    return curr * amount;
 }
 
-convert(500, usdCurr);
-convert(500, euroCurr);
+function promotion(result) {
+    console.log(result * discount);
+}
+
+const res = convert(500, usdCurr);
+
+promotion(res);
+
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if(i === 3) return;
+    }
+
+    console.log("Done");
+}
+
+test();
+
+
+function doNothing() {}
+
+console.log(doNothing() === undefined);
+
+
+
+/**
+ * Lesson 27 - Homework
+ */
+function getMathResult(base, count) {
+    let result = '';
+
+    if (typeof count === 'number') {
+        for (let i = 1; i <= count; i++) {
+            if (i != count) {
+                result += `${(base * i)}---`;
+            } else if(i == 1) {
+                result += `${base}`;
+            } else {
+                result += base * i;
+            }
+            
+        }
+    } else {
+        return base;
+    }
+
+    return result;
+}
+
+
+console.log(getMathResult(2, ''));
