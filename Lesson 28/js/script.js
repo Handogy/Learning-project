58b92d9974237ac30b5c04e9c531e648ee9cb21b
Dmytro <dmitry.khandogy@gmail.com>
2023-02-03 13:26:58 +0100

    Lesson 28 - done

--- Lesson 28/js/script.js
+++ Lesson 28/js/script.js
@@ -38,3 +38,91 @@ console.log(Math.round(num));
 const test = '12.2px';
 console.log(parseInt(test));
 console.log(parseFloat(test));
+
+
+/**
+ * Lesson 28
+ */
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+    
+            console.log('Done!');
+        } else{
+            console.log('Error!');
+    
+            i--;
+        }
+        
+    }
+}
+
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+    if(personalMovieDB.count < 10){
+        alert('Просмотрено довольно мало фильмов!');
+    } else if (personalMovieDB.count < 31) {
+        alert('Вы классический зритель!');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман!');
+    } else{
+        alert('Произошла ошибка!');
+    }
+}
+
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+// showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres(times) {
+    for (let i = 1; i <= times; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+    
+        if (genre != null && genre != '') {
+            personalMovieDB.genres[i] = genre;
+    
+            console.log('Done!');
+        } else{
+            console.log('Error!');
+    
+            i--;
+        }
+        
+    }
+}
+
+writeYourGenres(3);
+
+console.log(personalMovieDB);
