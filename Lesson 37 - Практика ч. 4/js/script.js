20ea3d7cbb71422f70d11a1f5cf3da3d75616e75
Dmytro <dmitry.khandogy@gmail.com>
2023-02-20 17:05:32 +0100

    Lesson 37

--- Lesson 37 - Практика ч. 4/js/script.js
+++ Lesson 37 - Практика ч. 4/js/script.js
@@ -117,3 +117,147 @@ const personalMovieDB = {
 // personalMovieDB.toggleVisibleMyDB();
 
 // console.log(personalMovieDB);
+
+
+// Упражнение по написанию кода 13
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    let generalSquard = 0,
+        generalValue = 0,
+        generalSum = 0;
+
+    data.shops.forEach(element => {
+        generalSquard += element.width * element.length;
+    });
+
+    generalValue = generalSquard * data.height;
+
+    generalSum = generalValue * data.moneyPer1m3;
+    
+    if (generalSum > data.budget) {
+        return 'Бюджета недостаточно';
+    } else {
+        return 'Бюджета достаточно';
+    }
+}
+
+console.log(isBudgetEnough(shoppingMallData));
+
+
+/**
+ * Ivan's solution
+ */
+// const shoppingMallData = {
+//     shops: [
+//         {
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000
+// }
+
+// function isBudgetEnough(data) {
+//     let square = 0;
+//     let volume = 0;
+
+//     data.shops.forEach(shop => {
+//         square += shop.width * shop.length;
+//     });
+
+//     volume = data.height * square;
+
+//     if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+//         return 'Бюджета достаточно';
+//     } else {
+//         return 'Бюджета недостаточно';
+//     }
+// }
+
+// isBudgetEnough(shoppingMallData);
+
+
+// ====================================
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', 'Somebody'];
+
+function sortStudentsByGroups(arr) {
+    let newArr = arr.sort();
+    let result = [];
+
+    for (let i = 0; i < 3; i++) {
+        result.push(newArr.slice(0, 3));
+        newArr = newArr.slice(3);
+    }
+
+    if (newArr.length > 0) {
+        result.push(`Оставшиеся студенты: ${newArr.join(", ")}`);
+    } else{
+        result.push(`Оставшиеся студенты: -`);
+    }
+
+    return result;
+}
+
+console.log(sortStudentsByGroups(students));
+
+
+// Решение Ивана
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+// function sortStudentsByGroups(arr) {
+//     arr.sort();
+//     const a = [], b = [], c = [], rest = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i < 3) {
+//             a.push(arr[i]);
+//         } else if (i < 6) {
+//             b.push(arr[i]);
+//         } else if (i < 9) {
+//             c.push(arr[i]);
+//         } else {
+//             rest.push(arr[i]);
+//         }
+//     }
+//     return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+// }
+
+// sortStudentsByGroups(students);
