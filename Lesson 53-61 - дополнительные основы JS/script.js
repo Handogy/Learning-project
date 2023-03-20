3ed45ca6c0bfb8feac7e7a0c2a6cc1b94db6be9b
Dmytro <dmitry.khandogy@gmail.com>
2023-03-20 14:10:07 +0100

    Lesson 53

---
+++ Lesson 53-61 - дополнительные основы JS/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+/**
+ * Оператор нулевого слияния реагирует только на Null и на Underfined
+ * 
+ * Использовать можно как: ??
+ */
+
+const box = document.querySelector('.box');
+
+const newHeight = 100;
+const newWidth = 400;
+
+function changeParams(elem, h, w) {
+    elem.style.height = `${h ?? 200}px`;
+    elem.style.width = `${w ?? 200}px`;
+    elem.innerHTML = (h ?? 200) * (w ?? 200);
+}
+
+changeParams(box, newHeight, newWidth);
+
+let userName;
+let userKey;
+
+console.log(userName ?? userKey ?? 'User');
