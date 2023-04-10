b8d9633a38ced8f6ac2d536e204aad0e1494c5ad
Dmytro <dmitry.khandogy@gmail.com>
2023-04-10 11:15:35 +0200

    Lesson 82 - JSON

---
+++ Lesson 82 - JSON/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const persone = {
+    name: 'Alex',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike',
+    }
+};
+
+const clone = JSON.parse(JSON.stringify(persone));
+
+clone.parents.mom = 'Ann';
+
+console.log(persone);
+console.log(clone);
