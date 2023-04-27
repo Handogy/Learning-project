fffc421c4cb9500eba37baeb0c2c4eed4ebd952c
Dmytro <dmitry.khandogy@gmail.com>
2023-04-27 20:02:29 +0200

    Lesson - 99. Геттеры и сеттеры (свойства объектов)

---
+++ Lesson 99 - Геттеры и сеттеры (свойства обьектов)/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** 
+ * Lesson 99. Геттеры и сеттеры (свойства объектов)
+ */
+const persone = {
+    name: "Alex",
+    age: 25,
+
+    get userAge() {
+        return this.age;
+    },
+
+    set userAge(num) {
+        this.age = num;
+    }
+};
+
+console.log(persone.userAge = 30);
+console.log(persone.userAge);
+
+
