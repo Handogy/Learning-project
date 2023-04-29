dee343572335ee6dfa1876e578e7d6f3d91bf950
Dmytro <dmitry.khandogy@gmail.com>
2023-04-29 16:32:03 +0200

    102. Webpack. Собираем наш проект

---
+++ Lesson 102 - Webpack. Собираем наш проект/src/js/main.js
@@ -0,0 +1,16 @@
+
+/** 
+ * Lesson 102. Webpack. Собираем наш проект
+ */
+
+function myModule() {
+    this.hello = function() {
+        console.log('hello');
+    };
+    
+    this.goodbye = function() {
+        console.log('bye!');
+    };
+}
+
+module.exsports = myModule;
