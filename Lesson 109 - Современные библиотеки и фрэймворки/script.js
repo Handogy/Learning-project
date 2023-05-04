68797693a71abecec91037b61f4c501805753215
Dmytro <dmitry.khandogy@gmail.com>
2023-05-04 23:47:34 +0200

    109. Современные библиотеки и фрэймворки

---
+++ Lesson 109 - Современные библиотеки и фрэймворки/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+/**
+ * Angular
+ * 
+ * - node.js
+ * - TypeScript
+ * - Webpack
+ * - Webpack
+ * - MVC pattern
+ * - Angular
+ * 
+ * 
+ * 
+ * React
+ * - JSX
+ * - Babel
+ * - Webpack
+ * - node.js
+ * - React
+ * 
+ * 
+ * Vue.js
+ * - Webpack
+ * - Vue
+ * - node.js
+ * 
+ */
